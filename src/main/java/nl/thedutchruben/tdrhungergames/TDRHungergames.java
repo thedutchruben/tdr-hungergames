9e2d8e7b8b7e90ffe126e17c8164b17c4847a479
Ruben de Roos <deroosruben@gmail.com>
2023-11-19 14:41:34 +0100

    init

---
+++ src/main/java/nl/thedutchruben/tdrhungergames/TDRHungergames.java
@@ -0,0 +1,17 @@
+package nl.thedutchruben.tdrhungergames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TDRHungergames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/nl/thedutchruben/tdrhungergames/TDRHungergamesPlugin.java
@@ -0,0 +1,17 @@
+package nl.thedutchruben.tdrhungergames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TDRHungergamesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
